ce0d6b02d5b50a9d5522a97be9c24afce67cb66c
vansh <vanshkashyap.003@gmail.com>
2026-05-31 00:41:56 +0530

    Adding the analyze button to the UI

--- app.py
+++ app.py
@@ -196,4 +196,4 @@ def predict():
 # -----------------------------------------------------------------------------
 if __name__ == '__main__':
     # Launch on http://127.0.0.1:5000
-    app.run(host='127.0.0.1', port=5000, debug=False)
+    app.run(host="0.0.0.0", port=5000, debug=True)
